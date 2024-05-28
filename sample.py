69b6266cdc34e49c90a89b0a0560fd98177fe3e7
sachinpjaj26 <sachinjoseph054@gmail.com>
2024-05-28 19:29:13 -0400

    updated sample code AB#27

---
+++ sample.py
@@ -0,0 +1,13 @@
+import logging as lg
+def div(a,b):
+    try:
+        lg.info(str(a)+"/"+str(b))
+        d = a/b
+        return d
+    except Exception as e:
+        print("check log")
+        lg.error("division by zero error")
+        lg.exception(str(e))
+        lg.critical("critical error")
+
+div(4,0)
