8531d4b4c25c2649bc03679b93db821cc66cd0c7
Jency Shijin <jshijin12@gmail.com>
2024-07-09 11:35:54 -0400

    Updating Docker file

--- api_server/app/__init__.py
+++ api_server/app/__init__.py
@@ -47,7 +47,7 @@ def intialize_app(configName='config'):
     # Initialize services with configuration
     data_service = DataService(app.config, mongo)
     data_processing_service = DataProcessing(app.config)
-    model_training_service = ModelTraining(app.config)
+    model_training_service = ModelTraining(app.config,mongo)
     eye_tracking_service = EyeTracking(app.config,data_service,data_processing_service)
 
     # db.init_app(app)
